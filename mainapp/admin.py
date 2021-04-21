fc98343e821aaa04003a49aa94a433804e533382
Sungat Kaparov <sungatkaparov8@gmail.com>
2021-04-21 21:30:09 +0600

    added: Models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from mainapp.models import *
+
+admin.site.register(Meeting)
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(MeetingRate)
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,37 +4,42 @@ from django.db import models
 class User(models.Model):
     firstname =models.CharField(max_length=150)
     lastname =models.CharField(max_length=150)
-    photo = models.ImageField
-    email = models.EmailField
+    photo = models.ImageField(upload_to='static/imgs', blank=True, null=True)
+    email = models.EmailField()
     password = models.CharField(max_length=150)
 
     def __str__(self):
         return self.firstname
 
 class Meeting(models.Model):
+    users = models.ManyToManyField('User')
     title = models.CharField(max_length=150)
-    participants = models.IntegerField
-    created_at = models.DateTimeField
-    start_at = models.DateTimeField
-    longtitude = models.FloatField
-    latitude = models.FloatField
-    headman = models.ForeignKey('User', on_delete=models.CASCADE)
-    category = models.ForeignKey('Category',on_delete=models.CASCADE)
-    is_expired = models.BooleanField
+    participants = models.IntegerField()
+    created_at = models.DateTimeField()
+    start_at = models.DateTimeField()
+    longtitude = models.FloatField()
+    latitude = models.FloatField()
+    headman = models.ForeignKey('User', on_delete=models.CASCADE, related_name='+')
+    is_expired = models.BooleanField()
 
     def __str__(self):
         return self.title
 
-class Category(models.Model):
+class Tag(models.Model):
     title = models.CharField(max_length=150)
+    meetings = models.ManyToManyField('Meeting')
+    users = models.ManyToManyField('User')
 
     def __str__(self):
         return self.title
 
-class Meeting_rate(models.Model):
-    user_id = models.OneToOneField('User', on_delete=models.CASCADE)
-    meeting_id = models.OneToOneField('Meeting', on_delete=models.CASCADE)
+class Comment(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    meeting = models.ForeignKey('Meeting', on_delete=models.CASCADE)
 
-class Meeting_participant(models.Model):
-    user_id = models.OneToOneField('User', on_delete=models.CASCADE)
-    meeting_id = models.OneToOneField('Meeting', on_delete=models.CASCADE)
+class MeetingRate(models.Model):
+    score = models.IntegerField()
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    meeting = models.ForeignKey('Meeting', on_delete=models.CASCADE)
+
