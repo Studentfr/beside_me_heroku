593c5380655b6fdb28018a4477ebd5058368b254
Sungat Kaparov <sungatkaparov8@gmail.com>
2021-04-22 00:46:04 +0600

    added: serializers.py, rest api, user crud and endpoints

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,10 @@ from mainapp import views
 
 urlpatterns = [
     path('', views.index),
+
+    path('user-list/', views.userList, name="user-list"),
+    path('user-detail/<int:pk>/', views.userDetail, name="user-detail"),
+    path('user-update/<int:pk>/', views.userUpdate, name="user-update"),
+    path('user-delete/<int:pk>/', views.userDelete, name="user-delete"),
+    path('user-create/', views.userCreate, name="user-create"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,50 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from mainapp import models
+from mainapp.serializers import UserSerializer
+
 
 @api_view(['GET'])
 def index(request):
     return Response("hello")
+
+
+@api_view(['GET'])
+def userList(request):
+    users = models.User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def userDetail(request, pk):
+    user = models.User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def userCreate(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def userUpdate(request, pk):
+    user = models.User.objects.get(id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def userDelete(request, pk):
+    user = models.User.objects.get(id=pk)
+    if user.id != 1:
+        user.delete()
+    return Response('User has been deleted successfully')
+
